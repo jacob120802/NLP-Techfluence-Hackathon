81308a922acb5de3f348609beac07f08e241b9af
Jacob Abraham <jacob120802@gmail.com>
2023-03-23 16:13:21 +0530

    home page commit

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import { FiMenu } from "react-icons/fi";
-import { Link } from "react-router-dom";
 import logo from '../assets/logo2.png'
 import { Link, useNavigate } from "react-router-dom";
 
