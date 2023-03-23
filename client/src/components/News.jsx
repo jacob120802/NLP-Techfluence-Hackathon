56b1fe6048e0f3439ab9cd5b8aea1f9f134b0b80
Jacob Abraham <jacob120802@gmail.com>
2023-03-24 04:11:19 +0530

    news update

--- client/src/components/News.jsx
+++ client/src/components/News.jsx
@@ -21,17 +21,24 @@ function News(props) {
 
     return (
         <>
-            <div>News</div>
-            {/* {
+            <div className="py-3 text-5xl md:text-7xl font-bold text-center my-6">News</div>
+            <div className=" w-screen h-screen flex flex-wrap gap-6 justify-center">
+            {
                 news.map((element) => {
                     return (
-                        <div key={element.id}>
-                            <h1>{element.title}</h1>
-                            <p>{element.description}</p>
+                        <div class="max-w-sm h-auto bg-zinc-100 rounded-xl shadow-2xl">
+                            <img src={element.urlToImage} className="w-full rounded-t-xl" alt="image"/>
+                            <h2 className="font-bold text-lg my-2 mx-4">{element.title}</h2>
+                            <p className="font-medium text-sm my-4 mx-4">{element.description}</p>
+                            <button className="bg-indigo-700 text-white font-bold p-4 flex justify-center mx-auto rounded-md my-2 hover:bg-indigo-800 hover:scale-105 duration-300"><a href={element.url} target="_blank">Read More</a></button>
+
+                            
+
                         </div>
                     )
                 })
-            } */}
+            }
+            </div>
         </>
     )
 }
