7becb22bec2760f80c0247925818879e017afc46
Jacob Abraham <jacob120802@gmail.com>
2023-03-24 05:55:10 +0530

    api

--- client/src/components/Chat.tsx
+++ client/src/components/Chat.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css';
 import { MainContainer, ChatContainer, MessageList, Message, MessageInput, TypingIndicator } from '@chatscope/chat-ui-kit-react';
 
-const API_KEY = "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";
+const API_KEY = "";
 // "Explain things like you would to a 10 year old learning how to code."
 const systemMessage = { //  Explain things like you're talking to a software professional with 5 years of experience.
     "role": "system", "content": "Explain things like you're talking to a software professional with 2 years of experience."
