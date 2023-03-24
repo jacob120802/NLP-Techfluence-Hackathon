02ac9896e924876bf745ef8fc15ffc919e6cfcd8
Jacob Abraham <jacob120802@gmail.com>
2023-03-24 08:23:40 +0530

    dashboard ui

--- client/src/components/ServiceCard.jsx
+++ client/src/components/ServiceCard.jsx
@@ -5,7 +5,7 @@ function ServiceCard({title,icon,description,src}) {
         <div className="xs:w-[300px] sm:w-[50vw] md:w-[30vw]" onClick={() => window.open(src)}>
             <div className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card">
             <div className="bg-tertiary rounded-[20px] min-h-[280px] flex justify-evenly items-center flex-col">
-                <img src={icon} alt={title} className="object-contain w-full"/>
+                <img height="50" src={icon} alt={title} className="object-contain w-full"/>
                 <h3 className="text-white text-[20px] font-bold text-center">{title}</h3>
                 <p className="text-white text-sm p-4">{description.substring(0,400)}</p>
             </div>
