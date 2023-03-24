46e4344c423c0e11fdb92a82f69836ed7d080137
Jacob Abraham <jacob120802@gmail.com>
2023-03-24 06:26:59 +0530

    minor changes

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -26,7 +26,7 @@ function Navbar() {
         <header className="border-b border-gray-300 py-4">
             <div className="flex items-center justify-between xl:max-w-7xl xl:mx-auto max-w-full px-[8%] flex-wrap w-full">
                 <span className='flex items-center'><img src={logo} width={100} height={45} alt="logo" />
-                <h1 className='text-3xl font-bold sm:text-4xl'>Medi-BOT</h1></span>
+                <h1 className='text-3xl font-bold sm:text-4xl'>MediBOT</h1></span>
                 <FiMenu className="lg:hidden block h-6 w-6 cursor-pointer" onClick={() => setOpen(!open)} />
                 <nav className={`lg:flex lg:items-center lg:w-auto w-full ${open ? "block" : "hidden"} mt-5 lg:mt-0`}>
                     <ul className="text-base text-gray-600 lg:flex lg:justify-center text-center">
