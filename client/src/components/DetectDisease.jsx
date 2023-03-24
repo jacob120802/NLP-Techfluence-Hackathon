2fa672cdc11babb34ec9da7b88c898784cc4c982
puneetmpatil <patilpuneet01@gmailcom>
2023-03-24 07:32:23 +0530

    Detect Disease Component Updated

--- client/src/components/DetectDisease.jsx
+++ client/src/components/DetectDisease.jsx
@@ -23,6 +23,7 @@ function DetectDisease(props) {
 
   const [prompt, setPrompt] = useState("")
   const [result, setResult] = useState("")
+  const [isPredicting, setIsPredicting] = useState(false)
 
   const handleChange = (e) => {
     setPrompt(e.target.value)
@@ -30,10 +31,13 @@ function DetectDisease(props) {
 
   const handleClick = async (e) => {
     e.preventDefault()
+    setResult("")
+    setIsPredicting(true)
     console.log(prompt)
     const url = "http://localhost:8000/api/chat/chatbot"
     try {
       const response = await axios.post(url, { text: prompt })
+      setIsPredicting(false)
       setResult(response.data.data.replace("/r", ""))
     }
     catch (err) {
@@ -44,8 +48,10 @@ function DetectDisease(props) {
 
   return (
     <div className="justify-center flex flex-col w-[50vw] mx-auto mt-10">
+      <h1 className="text-center text-3xl my-6 font-extrabold">Detect Disease</h1>
       <textarea type="text" className="text-sm rounded-md bg-gray-100 outline-2" onChange={handleChange} />
       <div>
+        <h2 className="my-6 text-2xl">Sample Responses</h2>
         {
           sampleResponses.map((response) => {
             return (
@@ -57,8 +63,20 @@ function DetectDisease(props) {
         }
       </div>
       <button className='px-2 py-3 bg-blue-600 mt-4 text-white' onClick={handleClick}>Submit</button>
+      {
+        isPredicting && <div className="flex items-center justify-center mt-4">
+          <div
+            className="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
+            role="status">
+            <span
+              className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]"
+            >Loading...</span
+            >
+          </div>
+        </div>
+      }
       {result &&
-        <div className='mt-4'>
+        <div className='my-6 min-h-[300px]'>
           Disease : {result}
         </div>
       }
